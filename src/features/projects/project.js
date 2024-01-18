d270f304114f860a6a3314cba5dce2df98873e57
Jawad Makary <jawadhalbymakary@gmail.com>
2024-01-18 11:46:57 +0200

    first commit

---
+++ src/features/projects/project.js
@@ -0,0 +1,38 @@
+
+import proj from "../../assets/images/proj.jpg";
+export const project = [
+  {
+    title: "Care 4 Health",
+    description:
+      "An informative website built with React.js for Care 4 Health Company, a leading provider of medical equipment and supplies in Lebanon.",
+    image: proj,
+    tags: ["react"],
+    demo: "https://care4health.com.lb/",
+  },
+  {
+    title: "Metaversus",
+    description:
+      " A website built with React.js and framer motion ",
+    image: proj,
+    tags: ["react", "framer motion"],
+    sourceCode: "https://github.com/JawadMakary/Metaversus",
+  },
+  {
+    title: "Zoom Clone",
+   description:'A Node.js-powered Zoom clone, offering seamless video conferencing with real-time communication features for a reliable and efficient virtual meeting experience.',
+    image: proj,
+    tags: ["Node JS", "EJS", "CSS"],
+     sourceCode: "https://github.com/JawadMakary/Zoom_Clone_NodeJs",
+  },
+  {
+    title: "Linkedin Clone",
+    description:
+      "A sleek and visually appealing design for a LinkedIn clone, combining modern aesthetics with intuitive user interfaces to enhance the overall user experience.",
+    image: proj,
+    tags: ["react", "redux"],
+    sourceCode: "https://github.com/JawadMakary/linkedInClone",
+    
+  },
+ 
+];
+
